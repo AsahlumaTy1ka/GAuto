dfe8cc98fc37e9b2342d12109548bb0ea19ca617
Teek4Y-X <137177560+AsahlumaTy1ka@users.noreply.github.com>
2025-01-29 12:19:35 +0000

    I dunno what am doing

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-from googleapiclient.discovery import build
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 import os
-import pickle
 import json
-from bs4 import BeautifulSoup
-from google.auth import credentials
 from mods import module
 
 blog_id = 7302333189972766248
@@ -14,20 +7,31 @@ blog_id = 7302333189972766248
 
 
 
-htmlcont =  module.genCont()
-#htmlcont = open('./file.html','r+')
-soup = BeautifulSoup(htmlcont, 'html.parser')
-post_title = soup.title.text
+#htmlcont =  module.genCont()
+htmlcont = open('./file.html','r+')
+markd = htmlcont.readlines()
+if markd[0] == '':
+  post_title = markd[1]
+else:
+  post_title = markd[0]
 Labls = module.genLabels(post_title=post_title)
 bannerPath = ""
 #bannerPath = module.generate_image(post_title=post_title)
-newTag = soup.new_tag('img',attrs={
-    "src":bannerPath,
-    "alt": post_title
-})
-soup.body.insert(1,newTag)
-#soup.body.insert(1,f'<img src="{bannerPath}" alt="{post_title}">')
 
+def UpperFi(post_title,Labls,fname):
+
+  upstr = f'''
+---
+layout: post
+title: {post_title}
+thumbnail-img: /assets/img/{fname}
+share-img: /assets/img/{fname}
+tags: {Labls}
+author: Asahluma Tyika
+---
+'''
+  return upstr  
+  
 # Define the SCOPES
 SCOPES = ['https://www.googleapis.com/auth/blogger']
 
@@ -44,109 +48,12 @@ def postCont(blog_id, title, content, schedule_post=False, schedule_time=None):
         schedule_time (str): Time to post if scheduling, in HH:MM (24-hour) format. Default is None.
     """
 
-def authenticate():
-    """Authenticates the user and returns API credentials."""
-    creds = None
-    
-    # Check if token.json exists
-    if os.path.exists('token.json'):
-        try:
-            # Load credentials from token.json
-            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-        except (json.JSONDecodeError, ValueError):
-            print("token.json is corrupted or invalid. Regenerating credentials...")
-    
-    # If no valid credentials, run the OAuth flow
-    if not creds or not creds.valid:
-        try:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'creds.json',
-                scopes=SCOPES
-            )
-            # Set redirect URI
-            flow.redirect_uri = "https://ideal-succotash-7gg7vg9jr5gcrjpr.github.dev/oauth2callback"
-            
-            # Generate the authorization URL
-            auth_url, _ = flow.authorization_url(
-                access_type='offline', prompt='consent'
-            )
-            print('Please go to this URL to authorize the app: {}'.format(auth_url))
-            
-            # Manually fetch the authorization code
-            code = input('Enter the authorization code: ')
-            
-            # Exchange the authorization code for tokens
-            flow.fetch_token(code=code)
-            creds = flow.credentials
-            # Save credentials to token.json
-            with open('token.json', 'w') as token:
-                token.write(creds.to_json())
-        except Exception as e:
-            print(f"Error during authentication: {e}")
-            return None
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-    
-    return creds
 
-  
-            
-def get_authenticated_service():
-  """
-  Gets authenticated service using refresh token.
-
-  Returns:
-    The authenticated Blogger API service object.
-  """
-  try:
-    with open('token.json', 'r') as token_file:
-      creds_data = json.load(token_file)
-      creds = credentials.Credentials(**creds_data, scopes=SCOPES)
-
-    if not creds.valid:
-      if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-      else:
-        raise Exception('Invalid credentials. Please re-authenticate.')
-
-    return build('blogger', 'v3', credentials=creds)
-
-  except FileNotFoundError:
-    print("Error: 'token.json' file not found.")
-    return None
-  except Exception as e:
-    print(f"An error occurred: {e}")
-    return None
-
-# Example usage:
-
-
-def create_post(service, blog_id, title, content,lbls):
-    """Creates a new post on the specified Blogger blog."""
-    post = {
-        "kind": "blogger#post",
-        "title": title,
-        "content": content,
-        "labels": lbls
-    }
-    request = service.posts().insert(blogId=blog_id, body=post)
-    response = request.execute()
-    print("Post title :"+title)
-    print("Post created successfully:", response['url'])
-
-# Authenticate and build the Blogger service
-crds = authenticate()
-#print(f"crds {crds}")
-sevice = build('blogger', 'v3', credentials=crds)
-#sevice = get_authenticated_service()
-# Post immediately
-post_body = str(soup.body)
-post_body = post_body.replace('<body>', '')
-post_body = post_body.replace('</body>', '')
+def create_post(markdown,img_name):
+   pass
 
 print(f"Labels : {Labls}")
 #print(f"Banner Path : {bannerPath}")
-#print(f"POst body :{post_body}")
 create_post(sevice, blog_id, post_title, post_body,Labls)
 
 
--- mods/module.py
+++ mods/module.py
@@ -6,7 +6,7 @@ import random
 import requests
 load_dotenv()
 myk = os.getenv('API_KEY')
-genai.configure(api_key='AIzaSyAMd-EzTIfn7rh0juAwBFJk3LR6pYOUA6E')
+genai.configure(api_key='')
 
 # Generate an image
 def generate_image(post_title,guidance_scale=4, height=512, width=512, max_sequence_length=256, num_images=1, num_inference_steps=15, seed=1):
@@ -27,7 +27,7 @@ def generate_image(post_title,guidance_scale=4, height=512, width=512, max_seque
     Returns:
         dict: The JSON response from the API.
     """
-    output_path = './banners/'+str(''.join(random.choices(string.ascii_letters,k=7)))+ '.jpg'
+    fname = './banners/'+str(''.join(random.choices(string.ascii_letters,k=7)))+ '.jpg'
     token = os.getenv("AI_AKEY")
     print('AI_KEY : '+ token)
     prompt = f"A blog post banner with title '{post_title}'"
@@ -63,9 +63,9 @@ def generate_image(post_title,guidance_scale=4, height=512, width=512, max_seque
         r = response.json()
         imgUrl = r['outputs'][0]['value'][0]['file']['url']
         img_data = requests.get(imgUrl).content
-        with open(output_path, 'wb') as handler:
+        with open(fname, 'wb') as handler:
             handler.write(img_data)
-        return output_path
+        return fname
         #return response.json()
     else:
         return {"error": response.status_code, "message": response.text}
@@ -74,14 +74,14 @@ def generate_image(post_title,guidance_scale=4, height=512, width=512, max_seque
 
 def genCont():
     model = genai.GenerativeModel('gemini-1.5-flash')
-    prompt = '''Generate me a blog post about a random coding tutorial people or coders usually search about,markdown format and make it as long as possible and do not take this as a chat plus dont put the markdown in a codeblock just return plain html and use  as codeblocks instead of code tags'''
+    prompt = '''Generate me a blog post about a random coding tutorial people or coders usually search about,markdown format and make it as long as possible(1000 words min) and do not take this as a chat plus dont put the markdown in a codeblock just return plain markdown and use {% highlight javascript linenos %} var foo = function(x) { return(x + 5); } foo(3) {% endhighlight %} as codeblocks(this is an example) instead of code tags'''
     resp = model.generate_content(prompt)
     return resp.text
 
 
 
 def genLabels(post_title):
-    lblPrompt = f'Generate me labels for post with title {post_title} and return me a list,do not put it on code blocks ,return it like this example : Label1,Label2,Labeln . and it should not be greater than 5'
+    lblPrompt = f'Generate me labels for post with title {post_title} and return me a list,do not put it on code blocks ,return it like this example :[Label1,Label2,Labeln . and it should not be greater than 5'
     model = genai.GenerativeModel('gemini-1.5-flash')
     labels = model.generate_content(lblPrompt)
     return labels.text
